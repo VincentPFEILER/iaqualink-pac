e0455137bc703d51892eed98fc51861091c216ff
Vincent PFEILER <105043929+VincentPFEILER@users.noreply.github.com>
2024-12-29 15:56:40 +0100

    Update api.py

--- custom_components/   zodiac_pac/api.py
+++ custom_components/   zodiac_pac/api.py
@@ -1,30 +1,25 @@
 import requests
+from .const import API_URL, API_KEY
 
-BASE_URL = "https://prod.zodiac-io.com"
+class ZodiacAPI:
+    """Handle communication with the Zodiac API."""
 
-class ZodiacPacAPI:
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, email, password):
+        self.email = email
         self.password = password
-        self.token = None
+        self.id_token = None
 
     def authenticate(self):
-        url = f"{BASE_URL}/login"
-        payload = {"username": self.username, "password": self.password}
+        """Authenticate the user and retrieve the token."""
+        url = f"{API_URL}/users/v1/login"
+        payload = {
+            "apiKey": API_KEY,
+            "email": self.email,
+            "password": self.password,
+        }
         response = requests.post(url, json=payload)
         if response.status_code == 200:
-            self.token = response.json().get("token")
+            data = response.json()
+            self.id_token = data.get("IdToken")
         else:
             raise Exception("Authentication failed")
-
-    def get_pac_info(self):
-        if not self.token:
-            self.authenticate()
-        url = f"{BASE_URL}/api/v1/pac"
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception("Failed to fetch PAC info")
-
