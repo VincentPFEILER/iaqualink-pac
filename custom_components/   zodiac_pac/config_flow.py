582579eb2e11f3097950b0d38ac664e0a1d472f2
Vincent PFEILER <105043929+VincentPFEILER@users.noreply.github.com>
2024-12-29 15:56:18 +0100

    Update config_flow.py

--- custom_components/   zodiac_pac/config_flow.py
+++ custom_components/   zodiac_pac/config_flow.py
@@ -1,33 +1,36 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from .api import ZodiacAPI
 from .const import DOMAIN
 
-class ZodiacPacConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle the configuration flow for the iAqualink integration."""
+
     async def async_step_user(self, user_input=None):
+        """Handle the first step of the configuration."""
         errors = {}
 
         if user_input is not None:
-            username = user_input["username"]
-            password = user_input["password"]
-
-            # Testez la connexion ici (API call)
-            if await self.test_credentials(username, password):
-                return self.async_create_entry(title="Zodiac PAC", data=user_input)
-            else:
+            api = ZodiacAPI(user_input["email"], user_input["password"])
+            try:
+                await self.hass.async_add_executor_job(api.authenticate)
+                return self.async_create_entry(
+                    title="iAqualink",
+                    data={
+                        "email": user_input["email"],
+                        "password": user_input["password"],
+                    },
+                )
+            except Exception:
                 errors["base"] = "auth_failed"
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required("username"): str,
+                    vol.Required("email"): str,
                     vol.Required("password"): str,
                 }
             ),
             errors=errors,
         )
-
-    async def test_credentials(self, username, password):
-        # Remplacez par un appel réel à l'API pour vérifier les identifiants
-        return username == "test" and password == "test"
-
