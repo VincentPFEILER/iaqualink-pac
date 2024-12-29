57ffa5431305c0c15edcf22e1fffb2af3d66f930
Vincent PFEILER <105043929+VincentPFEILER@users.noreply.github.com>
2024-12-29 16:56:44 +0100

    Update __init__.py

--- custom_components/   zodiac_pac/__init__.py
+++ custom_components/   zodiac_pac/__init__.py
@@ -1,14 +1,18 @@
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the integration via YAML (not supported for this integration)."""
+    _LOGGER.debug("async_setup called for Zodiac PAC")
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the integration from a config entry."""
+    _LOGGER.debug("async_setup_entry called for Zodiac PAC with data: %s", entry.data)
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
     hass.async_create_task(
@@ -18,6 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload the integration."""
+    _LOGGER.debug("async_unload_entry called for Zodiac PAC")
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
