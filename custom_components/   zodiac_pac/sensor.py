1774a32e835cff6744fc8f4e95a9f36c4a504c55
Vincent PFEILER <105043929+VincentPFEILER@users.noreply.github.com>
2024-12-29 15:49:36 +0100

    Update sensor.py

--- custom_components/   zodiac_pac/sensor.py
+++ custom_components/   zodiac_pac/sensor.py
@@ -1,1 +1,26 @@
+from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    api = ZodiacPacAPI(
+        config_entry.data["username"],
+        config_entry.data["password"]
+    )
+    async_add_entities([ZodiacPacSensor(api)])
+
+class ZodiacPacSensor(SensorEntity):
+    def __init__(self, api):
+        self.api = api
+        self._state = None
+
+    @property
+    def name(self):
+        return "Zodiac PAC"
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_update(self):
+        self._state = self.api.get_pac_info().get("current_temperature")
 
